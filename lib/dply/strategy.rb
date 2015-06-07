c1f3aadc85827f931c15bc8f835d41340d60af50
Neeraj Bhunwal <neeraj.bhunwal@gmail.com>
2015-06-07 16:48:43 +0530

    v0.2.0 persist roles passed to dplyr

--- lib/dply/strategy.rb
+++ lib/dply/strategy.rb
@@ -5,8 +5,37 @@ module Dply
       require_relative "strategy/#{config.strategy}"
       const = "::Dply::Strategy::#{config.strategy.capitalize}"
       const = Module.const_get(const)
+
+      # persist roles if DPLY_PERSIST_ROLES env is present
+      # load roles from roles file
+      roles = get_roles
+      persist_roles(roles)
+
       return const.new(config, options)
     end
+
+    private
+
+    def self.persist_roles(roles)
+      persist = ENV['DPLY_PERSIST_ROLES']
+      return if not persist
+      return if not roles
+      Logger.logger.info  "persisting roles #{roles}"
+      File.open('roles', 'w') { |f| f.write roles }
+    end
+
+    def self.get_roles
+      ENV['DPLY_ROLES'] ||= roles_from_file
+    end
+
+    def self.roles_from_file
+      if File.readable? "roles"
+        File.read("roles").chomp.strip
+      else
+        nil
+      end
+    end
+
 
   end
 end
