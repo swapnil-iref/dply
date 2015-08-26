9dc6e0680321e2281172a7fd47ea8e401c28d109
Neeraj Bhunwal <neeraj.bhunwal@gmail.com>
2015-08-26 14:10:39 +0530

    move shell functions to helper

--- lib/dply/helper.rb
+++ lib/dply/helper.rb
@@ -1,18 +1,57 @@
-require 'dply/shell'
-require 'dply/git'
 require 'dply/error'
+require 'dply/logger'
+require 'fileutils'
+require 'tmpdir'
 
 module Dply
   module Helper
 
-    def self.git
-      Git
+    def cmd(command, display: true, error_msg: nil, return_output: false, env:{})
+      stringify_values!(env)
+      if display
+        logger.bullet command
+      else
+        logger.debug command
+      end
+      command_arr = command.split
+
+      output = if return_output
+        IO.popen(env, command_arr) { |f| f.read }
+      else
+        system(env, *command_arr, 2 => 1)
+      end
+      return_value = $?.exitstatus
+      error_msg ||= "non zero exit for \"#{command}\""
+      error error_msg if return_value != 0
+      return output
     end
 
-    include Shell
+    def symlink(src, dst)
+      if File.symlink? dst
+        Dir.mktmpdir("sym-", "./") do |d|
+          dst_tmp = "#{d}/#{File.basename dst}"
+          FileUtils.ln_s src, dst_tmp
+          cmd "mv #{dst_tmp} #{File.dirname dst}", display: false
+        end
+      elsif File.exist? dst
+        error "cannot create symlink #{dst} => #{src}"
+      else
+        FileUtils.ln_s src, dst
+      end
+    end
+
+    def stringify_values!(hash)
+      hash.each do |k,v|
+        hash[k] = v.to_s
+      end
+    end
+
+    def logger
+      Logger.logger
+    end
 
     def git
-      ::Dply::Helper.git
+      Git
     end
 
     def error(msg)
