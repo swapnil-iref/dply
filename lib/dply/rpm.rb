7ccad47fa71b4726f706688e9dcd5b27e9cb099c
Neeraj Bhunwal <neeraj.bhunwal@gmail.com>
2015-08-27 14:27:11 +0530

    rpm.rb: generate lib -> package map instead of packages list

--- lib/dply/rpm.rb
+++ lib/dply/rpm.rb
@@ -4,14 +4,16 @@ module Dply
   class Rpm
     include Helper
 
-    def libs_to_packages(libs)
-      packages = Set.new
-      libs.each do |l|
-        list = whatprovides(l)
-        packages << list if not list.empty?
+    def libs_packages_map(libs)
+      h = {}
+      libs.each do |lib|
+        list = whatprovides(lib)
+        h[lib] = list if not list.empty?
       end
-      return packages
+      return h
     end
+
+    private
 
     def filtered?(pkg)
       @filtered ||= ["glibc", "libgcc", "libstdc++", "openssl", "ruby-alt", "jemalloc"]
@@ -21,9 +23,7 @@ module Dply
     def whatprovides(lib)
       lib = "#{lib}()(64bit)"
       command = %(rpm --queryformat "%{NAME} " -q --whatprovides "#{lib}")
-      logger.debug command
-      output = `#{command}`
-      error "running command #{command}" if not $?.exitstatus == 0
+      output = cmd command, return_output: true, shell: true
       list = output.strip.split.select {|pkg| not filtered? pkg }
     end
 
